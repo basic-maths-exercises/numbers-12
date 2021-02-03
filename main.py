c3155eb8befef0ad597a1ec51e3c55b1d2727ea2
Gareth Tribello <gareth.tribello@gmail.com>
2021-02-03 18:49:08 +0000

    Added answers

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import numpy as np
 
 def getDecimalDigits( x ) : 
   # Your code goes here
-
+  x, vv = x - np.floor( x ), np.zeros(10)
+  for i in range(10) : 
+      p = 1/(10**(i+1))
+      vv[i] = np.floor( x / p )
+      x = x - vv[i]*p
+  return vv
 
 # You don't need to ajust the code here.  This allows you to test the function
 # that you have written above.
